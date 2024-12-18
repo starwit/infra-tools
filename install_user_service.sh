7d51f6b097edbc24df5a283a170e06ad5c32eb11
flonix8 <flstanek@googlemail.com>
2024-12-18 14:22:49 +0100

    Fix install script

--- install_user_service.sh
+++ install_user_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p .config/systemd/user
-sed "s:PATH_TO_SERVER_SCRIPT:$(pwd)/wol_http_server.py:" wol-http-trigger.service > .config/systemd/user/wol-http-trigger.service
+mkdir -p ~/.config/systemd/user
+sed "s:PATH_TO_SERVER_SCRIPT:$(pwd)/wol_http_server.py:" wol-http-trigger.service > ~/.config/systemd/user/wol-http-trigger.service
